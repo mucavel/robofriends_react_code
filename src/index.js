78abca52c7fc15b31edb2e5740239764a0286671
salvadormucavel <salvadormuca7@gmail.com>
2022-05-19 18:38:50 +0200

    adicao do codigo

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import './index.css';
+import App from './containers/App';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+reportWebVitals();
